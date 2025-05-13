c9c31fffa4257cbc9e3aadf679d2c9a045cb3200
sehyun0518 <rla6170@naver.com>
2025-05-13 04:08:55 +0000

    fix: change image png -> svg

--- src/features/login/components/Logo.tsx
+++ src/features/login/components/Logo.tsx
@@ -1,13 +1,13 @@
-import FestivalLogo from "@/assets/images/logo.png";
-import LikeLionLogo from "@/assets/images/logo_like-lion.png";
-import CrossIcon from "@/assets/icons/cross.svg?react";
-import * as S from "./Logo.styles";
+import FestivalLogo from '@/assets/images/logo.svg';
+import LikeLionLogo from '@/assets/images/logo_like-lion.svg';
+import CrossIcon from '@/assets/icons/cross.svg?react';
+import * as S from './Logo.styles';
 
 export default function Logo() {
   return (
     <S.Container>
       <S.FestivalLogo src={FestivalLogo} alt="Festival Logo" />
-      <CrossIcon width={"1rem"} height={"1rem"} />
+      <CrossIcon width={'1rem'} height={'1rem'} />
       <S.LikeLionLogo src={LikeLionLogo} alt="Like Lion Logo" />
     </S.Container>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '@/styles/global';
 import { theme } from '@/styles/theme';
-import {
-  Login,
-  Admin,
-} from '@/pages';
+import { Login, Admin } from '@/pages';
 import Layout from '@/layout';
 
 const routes = createBrowserRouter([
@@ -23,7 +20,7 @@ const routes = createBrowserRouter([
       {
         path: '/admin',
         element: <Admin />,
-      }
+      },
     ],
   },
 ]);
